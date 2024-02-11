0c0f09996bce8df0f23c93c1fde0e0e0ccd2c89a
varfgehu <63542132+varfgehu@users.noreply.github.com>
2024-02-11 10:18:16 +0000

    adding basic spring (#1)

---
+++ src/test/java/com/example/dummyapi/DummyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
